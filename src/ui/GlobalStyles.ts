c8fdc2dac05d505b5b702d5c1f3476110be5e1d9
Alex Kurov <Alex>
2022-08-03 16:45:24 +0300

    refactor(react-budget): add global tyles

--- src/ui/GlobalStyles.ts
+++ src/ui/GlobalStyles.ts
@@ -6,4 +6,16 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
 }
+HTML {
+    max-width: 100%;
+    background-color: #1E3542;
+}
+body {
+    max-width: 375px;
+    margin: 0 auto;
+    font-family: 'Source Sans Pro', sans-serif;
+    font-weight: 400;
+    color: #000000;
+    background: #FFFFFF;
+}
 `
