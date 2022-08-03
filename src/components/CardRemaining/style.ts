e68e6058f58aa22110077e2d8f8396c136f7a917
Alex Kurov <Alex>
2022-08-03 16:48:54 +0300

    feat(react-budget): add CardRemaining component wuth style

---
+++ src/components/CardRemaining/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { StyledCardBudget, Title } from "../CardBudget/style";
+
+export const StyledCardRemaining = styled(StyledCardBudget)`
+    justify-content: flex-start;
+    background-color: #CCD5FF;
+`
+export const RemainingTitle = styled(Title)`
+    margin-left: 30px;
+`
