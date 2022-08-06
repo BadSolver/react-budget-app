051de14642eeba7ace9238a53643ac830c2ed2ea
Alex Kurov <Alex>
2022-08-06 20:43:05 +0300

    refactor(react-budget): resolve all conflicts

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,10 +1,8 @@
 import { Badge } from "../Badge/Badge"
 import { Icons, StyledListItem, Title } from "./style"
 import close from '../../assets/icons/close.svg'
-<<<<<<< HEAD
 import { useExpensesContext } from "../../context"
-=======
->>>>>>> 92cc0b2b59a6a1ba65fa3abc0b79a9cfb6eb7b4e
+
 
 interface IProps {
     text: string,
@@ -12,7 +10,6 @@ interface IProps {
 }
 
 export const ListItem = ({text, cost}: IProps) => {
-<<<<<<< HEAD
 
   const {expenses} = useExpensesContext()
 
@@ -33,13 +30,11 @@ export const ListItem = ({text, cost}: IProps) => {
         <Badge cost={cost}></Badge>
         <Icons src={close}></Icons> */}
     </>
-=======
-  return (
-    <StyledListItem>
-        <Title>{text}</Title>
-        <Badge cost={cost}></Badge>
-        <Icons src={close}></Icons>
-    </StyledListItem>
->>>>>>> 92cc0b2b59a6a1ba65fa3abc0b79a9cfb6eb7b4e
+  // return (
+  //   <StyledListItem>
+  //       <Title>{text}</Title>
+  //       <Badge cost={cost}></Badge>
+  //       <Icons src={close}></Icons>
+  //   </StyledListItem>
   )
 }
