49ba08516ea91a424c5d351d0221f41a325439fb
Alex Kurov <Alex>
2022-08-07 18:37:31 +0300

    feat(react-budget): add context for expenses

--- src/context/ExpensesContext/ExpensesContext.tsx
+++ src/context/ExpensesContext/ExpensesContext.tsx
@@ -1,5 +1,42 @@
-import { createContext } from "react";
+import { createContext, FC, ReactNode, useContext, useState } from "react";
 
-export const ExpensesContext = createContext({
-    
+interface IExpense {
+    id: number,
+    body: string,
+    cost: number
+}
+
+interface IExpensesContext {
+    expenses: IExpense[]
+} 
+
+export const ExpensesContext = createContext<IExpensesContext>({
+    expenses: []
 })
+
+// export const useExpensesContext = () => useContext<IExpensesContext>(ExpensesContext)
+
+const useExpensesValue = () => {
+    const [expensesContext, setExpensesContext] = useState<IExpensesContext>(() => {
+        return {
+            expenses: [],
+            setExpenses: (value: IExpense) => {
+                setExpensesContext((context) => {
+                    return {
+                        ...context,
+                        expenses: [...context.expenses, value]
+                    }
+                })
+            }
+        }
+    })
+    return expensesContext
+}
+
+export const ExpensesProvider: FC<{children: ReactNode}> = ({children}) => {
+    return (
+        <ExpensesContext.Provider value={useExpensesValue()}>
+            {children}
+        </ExpensesContext.Provider>
+    )
+}
